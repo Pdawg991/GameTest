7e6c931fa20b41af7ee6a6074fbff6d283cbfcfc
Pdawg991 <106412178+Pdawg991@users.noreply.github.com>
2023-05-27 15:03:48 -0400

    Create Form.cs

---
+++ Form.cs
@@ -0,0 +1,37 @@
+using InteractiveNovel;
+
+namespace InteractiveNovelWithForm
+{
+	public partial class Form1 : Form
+	{
+		Data data = new Data(); public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			System.Environment.Exit(1);
+		}
+	}
+}
